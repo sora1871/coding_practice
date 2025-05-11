85cbc629c23fb38e221ae313f3fd5d8b85f40a43
sora1871 <kawauso6549@gmail.com>
2025-05-11 14:37:23 +0900

    feat: Add brute force and hashmap solutions for Two Sum

---
+++ problems/two_sum/dict.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self,nums: List[int],target: int) -> List[int]:
+        num_map = {} #空の辞書を作成
+        for i, num in enumerate(nums): #インデックスと値を同時に取り出す
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement],i]
+            num_map[num] = i
+
+
