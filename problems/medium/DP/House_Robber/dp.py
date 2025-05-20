7caa8f0772003515b2a48ec2d23b6c9c407fab97
sora1871 <kawauso6549@gmail.com>
2025-05-20 20:57:50 +0900

    house robber

---
+++ problems/medium/DP/House_Robber/dp.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 0:
+            return 0
+        if n == 1:
+            return nums[0]
+        if n == 2:
+            return max(nums[0], nums[1])
+
+        dp = [0] * (n + 1)
+        dp[1] = nums[0]
+        dp[2] = max(nums[0], nums[1])
+
+        for i in range(3, n + 1):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i - 1])
+
+        return dp[n]
